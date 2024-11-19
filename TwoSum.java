4b24e7ebbe88d8f3632dd858a230d46e9f86fc48
Duong <ntduong071200@gmail.com>
2024-11-19 23:23:32 +0700

    Week 1: Two Sum - Easy - Optimized Solution

--- TwoSum.java
+++ TwoSum.java
@@ -28,9 +28,9 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> result = new HashMap<>();
         for (int i = 0; i < nums.length; i ++) {
-            int value = target - nums[i];
-            if (result.containsKey(value)) {
-                return new int[]{result.get(value), i};
+            int complement = target - nums[i];
+            if (result.containsKey(complement)) {
+                return new int[]{result.get(complement), i};
             }
             result.put(nums[i], i);
         }
