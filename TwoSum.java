2f5116e35930fb8ef30fe4c8df50edd4dfdab73a
Duong <ntduong071200@gmail.com>
2024-11-19 23:21:23 +0700

    Week 1: Two Sum - Easy - Original Solution

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,11 @@
 class Solution {
+    /**
+     * Original Solution: Brute Force
+     *  Using nested loop to check every pair of elements whether their sum equals the target.
+     *  Array length: n
+     *  Total comparisons: n * n
+     *  Time complexity: O(n^2)
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -6,6 +13,26 @@ class Solution {
                     return new int[]{i, j};
                 }
             }
+        }
+        return new int[]{};
+    }
+
+    /**
+     * Optimized Solution
+     * Using Hashmap, store the number as key and its index as value. Calculate the complement target - currentNum,
+     * and check the Hashmap whether the complement exist.
+     *  Array length: n
+     *  Total comparisons: n
+     *  Time complexity: O(n)
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            int value = target - nums[i];
+            if (result.containsKey(value)) {
+                return new int[]{result.get(value), i};
+            }
+            result.put(nums[i], i);
         }
         return new int[]{};
     }
