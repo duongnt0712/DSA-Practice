a9484f8e9b9f83db322d250e9d19225492babab2
Duong <ntduong071200@gmail.com>
2024-11-20 22:01:54 +0700

    Week 1: Contains Duplicate - Easy - Optimized Solution

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -15,4 +15,20 @@ class Solution {
         }
         return result;
     }
+
+    /**
+     * Using Set, the add() method will return the boolean when checking whether the number exist in the Set.
+     *  Time complexity: O(n)
+     */
+    public boolean containsDuplicate(int[] nums) {
+        boolean result = false;
+        Set<Integer> values = new HashSet<>();
+        for (int num : nums) {
+            if (!values.add(num)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
