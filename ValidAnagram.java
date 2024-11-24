edc996eb2dc600aa16828565626bac70c0a3968d
Duong <ntduong071200@gmail.com>
2024-11-24 21:51:43 +0700

    Week 1: ValidAnagram - Easy - Original Solution

---
+++ ValidAnagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * Parsing each String to char array then sorting and comparing the arrays.
+     */
+    public boolean isAnagram(String s, String t) {
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+
+        return Arrays.equals(arrS, arrT);
+    }
+}
